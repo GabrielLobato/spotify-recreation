61b16d8192c8566bd7ceb85e3c6c08fdc769d755
Gabriel Lobato <mrjumper360@gmail.com>
2024-02-01 02:27:39 -0300

    implementada pesquisa de vários itens

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const searchInput = document.getElementById('search-input');
+
 const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
-const original = document.getElementById('artist-card');
+const gridContainer = document.getElementById('grid-container');
 
 
-let artistNumber = 0;
+
+
 
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}&_sort=name&_order=asc`;
     fetch(url)
         .then((Response) => Response.json())
         .then(result => {
-            artistNumber = result.length;
-            console.log(artistNumber);
             displayResults(result);
         });
 }
@@ -20,8 +19,8 @@ function requestApi(searchTerm) {
 function displayResults(result) {
     let current = 1;
     resultPlaylist.classList.add('hidden');
-    // const artistName = document.getElementById('artist-name');
-    // const artistImage = document.getElementById('artist-img');
+
+    while(gridContainer.firstChild){gridContainer.removeChild(gridContainer.firstChild)}
 
     result.forEach((element) => {
         cloneComponent(current);
@@ -37,8 +36,8 @@ function displayResults(result) {
 }
 
 function cloneComponent(id){
+    const original = document.getElementById('artist-card');
     const clone = original.cloneNode(true);
-    const gridContainer = document.getElementById('grid-container');
     clone.id = `artist-card${id}`;
 
     gridContainer.appendChild(clone);
@@ -47,6 +46,7 @@ function cloneComponent(id){
 
 
 document.addEventListener('input', function() {
+    const searchInput = document.getElementById('search-input');
     const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '') {
         resultPlaylist.classList.remove('hidden');
