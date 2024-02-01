d6aed3fe023728e71790de573cf862de482112f5
Gabriel Lobato <mrjumper360@gmail.com>
2024-02-01 00:24:34 -0300

    adicionado pesquisar mais de 1 item de uma vez (experimental)

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
+const original = document.getElementById('artist-card');
+
+
+let artistNumber = 0;
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}&_sort=name&_order=asc`;
     fetch(url)
         .then((Response) => Response.json())
-        .then((result) => displayResults(result));
+        .then(result => {
+            artistNumber = result.length;
+            console.log(artistNumber);
+            displayResults(result);
+        });
 }
 
 function displayResults(result) {
+    let current = 1;
     resultPlaylist.classList.add('hidden');
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+    // const artistName = document.getElementById('artist-name');
+    // const artistImage = document.getElementById('artist-img');
 
     result.forEach((element) => {
+        cloneComponent(current);
+        const currentArtist = document.getElementById(`artist-card${current}`);
+        const artistName = currentArtist.querySelector('.artist-name');
+        const artistImage = currentArtist.querySelector('.artist-img');
         artistName.innerText = element.name;
         artistImage.src = element.urlImg;
+        current ++;
     });
 
     resultArtist.classList.remove('hidden');
+}
+
+function cloneComponent(id){
+    const clone = original.cloneNode(true);
+    const gridContainer = document.getElementById('grid-container');
+    clone.id = `artist-card${id}`;
+
+    gridContainer.appendChild(clone);
 }
 
 
